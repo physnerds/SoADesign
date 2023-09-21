cc259f7bac5b6feeb96f538008da0aa105576e14
physnerds <amitbashyal@gmail.com>
2023-09-21 16:08:28 -0500

    Add the streamer and pointer related functions

--- include/val.h
+++ include/val.h
@@ -5,13 +5,20 @@ namespace soa{
             myval(const T& init_val):_val(init_val){
             };
             ~myval(){}
-            T _val;
         SOA_HOST_DEVICE_INLINE  myval<T>& operator=(const T& v){
               _val = v;
               return *this;
         }
-
+        /*SOA_HOST_DEVICE_INLINE*/ myval<T> operator*() const{
+            return *_val;
+        }
+        //Define a custom stream operator Needed for cout, printf etc..
+        friend std::ostream& operator<<(std::ostream&os, const myval<T,offset>& obj){
+            os<<obj._val;
+            return os;
+        }
         private:
             char pad[offset];
+            T _val;
     };
 }
