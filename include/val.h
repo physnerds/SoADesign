1e685c53136449e7fc4b11ea9703470b0f04fc85
physnerds <amitbashyal@gmail.com>
2023-09-22 09:32:40 -0500

    Add a default constructor

--- include/val.h
+++ include/val.h
@@ -1,19 +1,20 @@
 namespace soa{
-  template<typename T, int offset = 128-sizeof(T)%128>
+  template<typename T, int bytealignment=128, int offset = bytealignment-sizeof(T)%bytealignment>
     class myval{
         public:
+            myval():_val(0){}
             myval(const T& init_val):_val(init_val){
-            };
+            }
             ~myval(){}
         SOA_HOST_DEVICE_INLINE  myval<T>& operator=(const T& v){
               _val = v;
               return *this;
         }
-        /*SOA_HOST_DEVICE_INLINE*/ myval<T> operator*() const{
+        SOA_HOST_DEVICE_INLINE myval<T> operator*() const{
             return *_val;
         }
         //Define a custom stream operator Needed for cout, printf etc..
-        friend std::ostream& operator<<(std::ostream&os, const myval<T,offset>& obj){
+        friend std::ostream& operator<<(std::ostream&os, const myval<T,bytealignment,offset>& obj){
             os<<obj._val;
             return os;
         }
