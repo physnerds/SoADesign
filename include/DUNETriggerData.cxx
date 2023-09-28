696500641bf79bf35bb5605dd5ba7b7cfad1f37a
physnerds <amitbashyal@gmail.com>
2023-09-28 10:01:43 -0500

    Trial on DUNETriggerDataModel

---
+++ include/DUNETriggerData.cxx
@@ -0,0 +1,10 @@
+#include "DUNETriggerData.h"
+
+namespace soa
+{
+/// @brief Default constructors and destructors...
+DUNETrigger::DUNETrigger(){}
+
+DUNETrigger::~DUNETrigger(){}  
+} // namespace soa
+ 
