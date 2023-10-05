8876bf47b154ccb261d40b40b15ba6c71e97becd
Amit Bashyal <amitbashyal@gmail.com>
2023-10-05 09:06:49 -0500

    Delete include directory

--- include/memory.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CUDACC__
-#define SOA_HOST_ONLY __host__
-#define SOA_DEVICE_ONLY __device__
-#define SOA_HOST_DEVICE_INLINE __host__ __device__ __forceinline__
-#else
-#define SOA_HOST_ONLY
-#define SOA_DEVICE_ONLY
-#define SOA_HOST_DEVICE_INLINE inline
-#endif
