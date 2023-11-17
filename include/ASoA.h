fe1e5f62816b18e67f9006a5f2ce9ee195bb8b34
physnerds <amitbashyal@gmail.com>
2023-11-17 11:16:50 -0600

    simple ASoA to persist in RNTuple

---
+++ include/ASoA.h
@@ -0,0 +1,34 @@
+#ifndef ASOA_H
+#define ASOA_H 1
+
+#include "soa.h"
+#include <vector>
+
+namespace soa{
+class ASoA{
+
+    public:
+    ASoA(){};
+    ~ASoA(){};
+    static const int soa_size = 500;
+    static const int subsoa_size = 500;
+    Generate_Arrays(msoa,
+                    Addvector(uint32_t, a);  
+                    Addvector(uint32_t, b);
+                      
+                    );
+
+    Generate_Arrays(asoa,
+                    Addvector(uint32_t, aa);
+                    AddSoA(msoa, subsoa);
+
+                );
+
+    private:
+    
+
+
+};
+
+}
+#endif
