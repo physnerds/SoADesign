007d4cda106e7bec1428f64007fe3c1c8bf44923
Amit Bashyal <amitbashyal@gmail.com>
2023-11-30 20:21:15 -0600

    Add few more functionalities

--- convert_tree_to_rntuple.cc
+++ convert_tree_to_rntuple.cc
@@ -29,7 +29,7 @@ using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
 constexpr char const* tree_name = "caf";
 constexpr char const* file_name = "../CAF_FHC_TTree.root";
 constexpr char const* file_name_rntuple = "CAF_FHC_RNtuple.root";
-
+constexpr char const* ofile_name = "test_file.root";
 void Convert(){
     std::unique_ptr<TFile> f(TFile::Open(file_name));
     assert(f && ! f->IsZombie());
@@ -68,10 +68,52 @@ void Convert(){
         if( i && i% 1000==0)
         std::cout<<" Wrote "<<i<<" entries "<<std::endl;
     }
+    std::cout<<"Wrote all "<<nEntries<<" entries"<<std::endl;
+
 
 }
 
+void ReadRNTuple(){
+    auto ntuple = RNTupleReader::Open(tree_name,file_name_rntuple);
+    ntuple->PrintInfo(ENTupleInfo::kStorageDetails);
+    ntuple->PrintInfo();
+
+  //  ntuple->Show(0);
+}
+
+void ReadTree(){
+    std::unique_ptr<TFile> f(TFile::Open(ofile_name));
+    assert(f && ! f->IsZombie());
+    auto tree = f->Get<TTree>(tree_name);
+    auto _cf = f->GetCompressionFactor();
+    const int entries = tree->GetEntries();
+
+    std::cout<<"Total Entries "<<entries<<std::endl;
+    std::cout<<"Compression Factor "<<_cf<<std::endl;
+
+}
+void WriteCAFTree(){
+    std::unique_ptr<TFile> f(TFile::Open(file_name));
+    assert(f && ! f->IsZombie());
+
+    //unique ptr to an empty RNTuple model without a default entry
+    auto model = RNTupleModel::CreateBare();
+    auto tree = f->Get<TTree>(tree_name);
+    TFile newfile(ofile_name, "recreate");
+    auto ntree = tree->CloneTree();
+    ntree->Print();
+    newfile.Write();
+}
 int main(int argc, char* argv[]){
-    Convert();
+    if(argc<2){
+        printf("Need 1 for %s \n 2 for %s \n 3 for %s \n 4 for %s \n",
+        "Convert()","WriteCAFTree()","ReadRNTuple()","ReadTree()");
+    }
+    else{
+    if(atoi(argv[1])==1) Convert();
+    if(atoi(argv[1])==2) WriteCAFTree();
+    if(atoi(argv[1])==3) ReadRNTuple();
+    if(atoi(argv[1])==4) ReadTree();
+    }
     return 1;
 }
